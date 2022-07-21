620a5522710ea3f5224bb6c48d16e13d3878aee3
Nunder <nathannunderwood@gmail.com>
2022-07-21 15:04:35 -0700

    working on prompt function

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,20 @@
 // Assignment Code
 var generateBtn = document.getElementById("#generate");
 
+function upperCase() {
+  prompt("Do you want to include capital letter(s)?");
+}
+
 // Write password to the #password input
 function writePassword() {
+  prompt("Please choose the length of your password.", "Must be between 8 and 128 characters");
+    // if (chooseLength.length < 8 || chooseLength > 128) {
+    //   alert("Password not correct length");
+    // } else {
+    //   function upperCase();
+  }
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -12,3 +24,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function createPassword() {
+  prompt("Please choose your password length", "Must be between 8 and 128 characters");
+}
