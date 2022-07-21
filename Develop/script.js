175a05d7fc6532b43d82858c2b35868e7b35cf27
Nunder <nathannunderwood@gmail.com>
2022-07-20 21:01:41 -0700

    another commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("#generate");
 
 // Write password to the #password input
 function writePassword() {
