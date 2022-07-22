2a70e931bbb7937dbf0d2189a25def8f361b6c0f
Nunder <nathannunderwood@gmail.com>
2022-07-21 22:32:31 -0700

    clean slate

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,8 @@
 // Assignment Code
-var generateBtn = document.getElementById("#generate");
-
-function upperCase() {
-  prompt("Do you want to include capital letter(s)?");
-}
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  prompt("Please choose the length of your password.", "Must be between 8 and 128 characters");
-  }
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -20,7 +12,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function createPassword() {
-  prompt("Please choose your password length", "Must be between 8 and 128 characters");
-}
