2ef9aa95d362451dbf12c969142cc413a2999d99
Nunder <nathannunderwood@gmail.com>
2022-07-22 15:13:12 -0700

    maybeeeeeeeeeeeeee

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,15 @@
 // Assignment Code
+var length;
+var lower;
+var upper;
+var numeric;
+var special;
+
 var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", lengthPrompt)
+generateBtn.addEventListener("click", generatePassword);
 
-function lengthPrompt () {
+function generatePassword () {
     var length = prompt("Please indicate the desired length of your password", "Must be between 8 and 128 characters");
   
     if (length === null) {
@@ -11,11 +17,11 @@ function lengthPrompt () {
   
     } else if (length < 8 || length > 128 || isNaN(length)) {
       alert("Please choose a number between 8 and 128");
-      lengthPrompt();
+      generatePassword();
   
     }  else {
-      console.log(length);
-      lowerPrompt();
+        alert(`Your password will be ${length} characters`);
+        lowerPrompt();
     }
   }
 
@@ -31,6 +37,12 @@ function lengthPrompt () {
   
     } else {
       console.log(lower);
+
+      if (lower === 'yes') {
+        alert('Your password will have lower-case letters');
+      } else {
+        alert('Your password will not have lower-case letters');
+      }
       upperPrompt();
     }
   }
@@ -47,6 +59,11 @@ function lengthPrompt () {
   
     } else {
       console.log(upper);
+      if (upper === 'yes') {
+        alert('Your password will have UPPER-CASE letters');
+      } else {
+        alert('Your password will not have UPPER-CASE letters');
+      }
       numericPrompt();
     }
   }
@@ -63,6 +80,11 @@ function lengthPrompt () {
   
     } else {
       console.log(numeric);
+      if (numeric === 'yes') {
+        alert('Your password will have numbers');
+      } else {
+        alert('Your password will not have numbers');
+      }
       specialPrompt();
     }
   }
@@ -78,19 +100,39 @@ function specialPrompt () {
       specialPrompt();
   
     } else {
-      console.log(numeric);
+      console.log(special);
+      if (special === 'yes') {
+        alert('Your password is special!');
+      } else {
+        alert('Your password is not special!');
+      }
+
       writePassword();
     }
-  }
+  } 
+
+
+
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// var lowerAlpha = 'abcdefghijklmnopqrstuvwxyz';
+// var upperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var numbers = '0123456789';
+// var symbols = '!@#$%^&*_-+=';
+
+// function showPrompts() {
+//     lowerCase = confirm("Do you want to use lower case letters?");
+//     upperCase = confirm("Do you want to use upper case letters?");
+//     numbers = confirm("Do you want to use numbers?");
+//     special = confirm("Do you want to use any special characters?");
+//   }
