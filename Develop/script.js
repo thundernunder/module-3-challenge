13d60b0d78db5e3f7d302d9c77329729ad242f74
Nunder <nathannunderwood@gmail.com>
2022-07-22 05:33:41 -0700

    Build logic for prompts. Figure out yes no logic for secondPrompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function thirdPrompt () {
+ var upper = prompt("Do you want UPPER CASE letters?", "yes or no");
+
+  var smallUpper = upper.toLowerCase;
+
+  if (smallUpper !== "yes" || smallUpper !== "no") {
+    alert(`please type "yes" or "no"`)
+    thirdPrompt();
+  } else {
+    fourthPrompt();
+  }
+
+
+}
+
+function secondPrompt () {
+  var lower = prompt("Do you want lower case letters?", "yes or no" );
+
+  var smallLower = lower.toLowerCase;
+
+  if (smallLower != "yes" || smallLower != "no") {
+    alert(`please type "yes" or "no"`)
+    secondPrompt();
+  } else {
+    thirdPrompt();
+  }
+}
+
 function firstPrompt () {
-  prompt("Please indicate the desired length of your password", "Must be between 8 and 128 characters");
+  var length = prompt("Please indicate the desired length of your password", "Must be between 8 and 128 characters");
+
+  console.log(length);
+
+  if (length < 8 || length > 128 || length === NaN) {
+    alert("Please choose a password length between 8 and 128 characters");
+    firstPrompt();
+
+  } else {
+    secondPrompt();
+  }
 }
 
 generateBtn.addEventListener("click", firstPrompt)
